b2ac2eafcd93f3926483bf51660c2f64c900dc2e
NinjaWazzzabi <amk1997@hotmail.se>
2018-01-17 14:39:58 +0100

    Fix usage of wrong axis in FDC

--- drone_system/flight_dynamics_controller/flight_dynamics_controller.ino
+++ drone_system/flight_dynamics_controller/flight_dynamics_controller.ino
@@ -5,10 +5,10 @@
 #include "Vector3f.h"
 #include "pid.h"
 
-#define THROTTLE_CH	1
-#define RUDDER_CH	2
-#define ELEVATOR_CH	3
-#define AILERON_CH	4
+#define THROTTLE_CH 1
+#define RUDDER_CH 2
+#define ELEVATOR_CH 3
+#define AILERON_CH 4
 
 #define MAX_SERVO_VALUE 2000
 #define MIN_SERVO_VALUE 1000
@@ -41,7 +41,7 @@ unsigned char bufferIndex;
 
 long lastHostAhrsUpdate;
 
-void setup() 
+void setup()
 {
 	//Serial
 	Serial.begin(9600);
@@ -50,7 +50,8 @@ void setup()
 	bufferIndex = 0;
 
 	// Sends it's desciption until it has gotten it back.
-	while (true) {
+	while (true)
+	{
 		// Start of heading
 		Serial.write(ASCII_SOH);
 		Serial.flush();
@@ -64,8 +65,10 @@ void setup()
 		Serial.flush();
 
 		delay(100);
-		if (Serial.available() > 3) {
-			if (Serial.readString().equals(desc)) {
+		if (Serial.available() > 3)
+		{
+			if (Serial.readString().equals(desc))
+			{
 				break;
 			}
 		}
@@ -85,28 +88,29 @@ void setup()
 	elevatorValue = 0;
 	aileronValue = 0;
 
-	while (true) {
-		if (ahrs.begin()) {
+	while (true)
+	{
+		if (ahrs.begin())
+		{
 			break;
 		}
 	}
 	ahrs.setExtCrystalUse(true);
 }
 
-
 long time = 0;
 
-void loop() 
+void loop()
 {
 	//Read command data if there's any
 	readCommands();
 
-	//Update the actual 
+	//Update the actual
 	imu::Vector<3> vector = ahrs.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-	// vec.y : -pitch
-	// vec.z : roll
-	// vec.x : yaw
+	// vector.y = pitch
+	// vector.z = -roll
+	// vector.x = yaw
 	actual.setX(vector.y());
 	actual.setY(-vector.z());
 	actual.setZ(vector.x());
@@ -123,9 +127,9 @@ void loop()
 	zPid.update(zError, currentTime);
 
 	//throttleValue = desired.getLength();
-	rudderValue += zError;
-	elevatorValue += yError;
-	aileronValue += xError;
+	rudderValue = zPid.getValue();
+	elevatorValue = xPid.getValue();
+	aileronValue = yPid.getValue();
 
 	//Do action to minimize error
 	setServoValue(THROTTLE_CH, throttleValue);
@@ -133,7 +137,8 @@ void loop()
 	setServoValue(ELEVATOR_CH, elevatorValue);
 	setServoValue(AILERON_CH, aileronValue);
 
-	if (millis() - lastHostAhrsUpdate > UPDATE_DELAY) {
+	if (millis() - lastHostAhrsUpdate > UPDATE_DELAY)
+	{
 		sendAhrs();
 		lastHostAhrsUpdate = millis();
 	}
@@ -144,44 +149,44 @@ void loop()
 	// Serial.println(deltaTime);
 }
 
-
 //Tested!
 bool serialError = false;
-void readCommands() {
+void readCommands()
+{
 	while (Serial.available() > 0)
 	{
 		char input = Serial.read();
-		if (input == ASCII_EOT) 
+		if (input == ASCII_EOT)
 		{
-			if (!serialError && bufferIndex == 4) 
+			if (!serialError && bufferIndex == 4)
 			{
 				parseFlightCommand(serialBuffer);
 			}
 			serialError = false;
 			bufferIndex = 0;
-		} 
+		}
 		else if (!serialError)
 		{
-			if (serialError || bufferIndex >= BUFFER_SIZE) 
+			if (serialError || bufferIndex >= BUFFER_SIZE)
 			{
 				serialError = true; //Buffer overflow!
-            } 
-			else 
+			}
+			else
 			{
 				serialBuffer[bufferIndex] = input;
 				bufferIndex++;
 			}
 		}
-		
 	}
 }
 
 //Tested!
-void parseFlightCommand(char *vector) {
-	float throttle = (float) (char) *vector;
-	float rudder = (float) (char) *(vector + 1);
-	float pitch = (float) (char) *(vector + 2);
-	float roll = (float) (char) *(vector + 3);
+void parseFlightCommand(char *vector)
+{
+	float throttle = (float)(char)*vector;
+	float rudder = (float)(char)*(vector + 1);
+	float pitch = (float)(char)*(vector + 2);
+	float roll = (float)(char)*(vector + 3);
 
 	desired.setX(pitch);
 	desired.setY(roll);
@@ -190,29 +195,30 @@ void parseFlightCommand(char *vector) {
 }
 
 //Tested!
-void sendAhrs() {
+void sendAhrs()
+{
 	Serial.print("<");
 	printFloat(actual.getX());
-	Serial.print(",");	
+	Serial.print(",");
 	printFloat(actual.getY());
-	Serial.print(",");		
+	Serial.print(",");
 	printFloat(actual.getZ());
-	Serial.print(">");	
+	Serial.print(">");
 	Serial.write(ASCII_EOT);
 	Serial.flush();
 }
 
 //Tested!
-void printFloat(float f) 
+void printFloat(float f)
 {
-	Serial.print(f,4);
+	Serial.print(f, 4);
 }
 
 /*
 id range: 0 to 15
 value range: -100 to 100
 */
-void setServoValue(int id, int value) 
+void setServoValue(int id, int value)
 {
 	//Todo Test with hardware to verify correct behaviour
 	long mappedValue = map(value, -100, 100, MIN_SERVO_VALUE, MAX_SERVO_VALUE);
